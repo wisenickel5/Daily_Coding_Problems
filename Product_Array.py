f063305e8962f1f2d060debd75cd9833642dfb7b
Dylan Alexander <dalexander@carterandclark.com>
2022-01-13 17:11:56 -0500

    Completed Product Arrays

---
+++ Product_Array.py
@@ -0,0 +1,49 @@
+# Given an array of integers, return a new array such that 
+# each element at index i of the new array is the product 
+# of all the numbers in the original array except the one at i.
+# Ex. [1, 2, 3, 4, 5] --> [120, 60, 40, 30, 24]
+
+def Product_Array(arr):
+    n = len(arr)
+    # Base Case
+    if (n == 1):
+        return arr
+
+    left = [0] * n
+    right = [0] * n
+    prod = [0] * n
+
+    left[0] = 1
+    right[n - 1] = 1
+
+    # Iterate forwards through the left array 
+    for i in range(1, n):
+        left[i] = arr[i - 1] * left[i - 1]
+
+    # Iterate backwards through the right array
+    for j in range(n - 2, -1, -1):
+        right[j] = arr[j + 1] * right[j + 1]
+
+    # Iterate through left & right arrays & 
+    # multiply elements
+    for i in range(n):
+        prod[i] = left[i] * right[i]
+
+    # Print the Left array
+    print("Left Array: ")
+    for i in range(n):
+        print(str(left[i]), end=" ")
+
+    # Print the Right array
+    print("\n\nRight Array: ")
+    for i in range(n):
+        print(str(right[i]), end=" ")
+
+    # Print the final array
+    print("\n\nResult Array: ")
+    for i in range(n):
+        print(str(prod[i]), end=" ")
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5]
+    Product_Array(arr)
