a54f90e5117e0fb54cd738ad7b39ee5a94e092ec
Dylan Alexander <dalexander@carterandclark.com>
2022-02-21 14:35:23 -0500

    Added recursive backtracking solution to PowerSet.py

--- python/PowerSet.py
+++ python/PowerSet.py
@@ -6,7 +6,7 @@ For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2
 You may also use a list or array to represent a set.
 '''
 
-def power_set(set: list) -> list:
+def power_set(nums: list[int]) -> list[list[int]]:
 	"""We first start by creating a 2D Array and then iterating through each element in the set.
 	Within each iteration of the set passed as an arguement, we iterate through each subset of 
 	the current power set and then create a copy of that subset. With the copy created, we append
@@ -15,14 +15,13 @@ def power_set(set: list) -> list:
 	we append the finalized extra_set to the power_set with the .extend() function.
 
 	Args:
-		set (list): Initial Set
+		set (list[int]): Initial Set
 
 	Returns:
-		list: Power Set
+		list[list[int]]: Power Set
 	"""
 	power_set = [[]] # The power set will always contain an empty set
-
-	for element in set:
+	for element in nums:
 		extra_sets = []
 		for subset in power_set:
 			subset_copy = [subset_element for subset_element in subset]
@@ -33,5 +32,28 @@ def power_set(set: list) -> list:
 
 	return power_set
 
+def rec_power_set(nums: list[int]) -> list[list[int]]:
+	"""Uses backtracking to create a power set from a given list of integers
+	More information on backtracking: https://cs.lmu.edu/~ray/notes/backtracking/
+
+	Args:
+		nums (list[int]): Initial set of integers
+
+	Returns:
+		list[list[int]]: Final power set
+	"""
+	def generate_subsets(i):
+		subsets.append(subset[:])
+		if i > len(nums):
+			return
+		for j in range(i + 1, len(nums)):
+			subset.append(nums[j])
+			generate_subsets(j)
+			del subset[-1]
+	subsets, subset = [], []
+	generate_subsets(-1)
+	return subsets
+
 if __name__ == "__main__":
 	print(power_set([1,2,3]))
+	print(rec_power_set([1,2,3]))
