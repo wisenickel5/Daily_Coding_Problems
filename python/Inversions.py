1110a18279021877c2d784627632db186e275ca8
Dylan Alexander <dalexander@carterandclark.com>
2022-02-27 22:55:26 -0500

    Added Merge Sort to Inversions.py

--- python/Inversions.py
+++ python/Inversions.py
@@ -8,71 +8,56 @@ faster than O(N^2) time. You may assume each element in the array is distinct.
 For example, a sorted list has zero inversions. 
 The array [2, 4, 1, 3, 5] has three inversions: (2, 1), (4, 1), and (4, 3). 
 The array [5, 4, 3, 2, 1] has ten inversions: every distinct pair forms an inversion.
-Solution inspired by: https://runzhuoli.me/2018/09/06/count-inversions-in-an-array.html
+Solution inspired by: https://medium.com/@ssbothwell/counting-inversions-with-merge-sort-4d9910dc95f0
 '''
 
-def brute_force_inversions(arr: list[int]) -> list[tuple([int, int])]:
-	sender = []
-	for i in range(len(arr) - 1):
-		for j in range(len(arr) - 1):
+def brute_force_inversions(arr: list[int]) -> tuple([ int, list[tuple([int, int])] ]):
+	sender, counter = [], 0
+	for i in range(len(arr)):
+		for j in range(len(arr)):
 			if arr[i] > arr[j]:
 				if i < j:
+					counter += 1
 					sender.append(tuple([arr[j], arr[i]]))
-	return sender
+	return tuple([counter, sender])
 
-def optimal_inversions(arr: list[int]) -> list[tuple([int, int])]:
-	return find_inversions(arr, [None]*len(arr), 0, len(arr)-1)
+def optimal_inversions(arr: list[int]) -> tuple([ int, list[tuple([int, int])] ]):
+	if len(arr) == 1:
+		return tuple([0, arr])
+	else:
+		sender, counter = [], 0
+		middle_idx = int(len(arr) / 2)
+		left_side = arr[:middle_idx]
+		right_side = arr[middle_idx:]
 
-def find_inversions(arr: list[int], temp: list[int], left_start: int, right_end: int) -> list[tuple([int, int])]:
-	if left_start >= right_end: # Base Case
-		return None
+		left_idx, left_side = optimal_inversions(left_side)
+		right_idx, right_side = optimal_inversions(right_side)
 
-	middle_idx = int((left_start + right_end) / 2)
-	left_inversions = find_inversions(arr, temp, left_start, middle_idx)
-	right_inversions = find_inversions(arr, temp, middle_idx + 1, right_end)
-	split_inversions = find_split_inversions(arr, temp, left_start, right_end)
-	return [left_inversions, right_inversions, split_inversions]
+		i, j = 0, 0
+		counter = 0 + left_idx + right_idx
 
-def find_split_inversions(arr: list[int], temp: list[int], left_start: int, right_end: int) -> list[tuple([int, int])]:
-	sender, count = [], 0
-	left_end_idx = int((left_start + right_end) / 2) # Also the middle index
-	lp = int(left_start)
-	rp = left_end_idx + 1
-	right_start = left_end_idx + 1
+		while i < len(left_side) and j < len(right_side):
+			if left_side[i] <= right_side[j]:
+				sender.append(left_side[i])
+				i += 1
+			else: 
+				sender.append(right_side[j])
+				j += 1
+				counter += len(left_side) - i
 
-	for i in range(right_end - left_start):
-		if lp > left_end_idx:
-			ls_temp = int(left_start + i)
-			rp += 1
-			temp[ls_temp] = arr[rp]
-		
-		elif rp > right_end:
-			ls_temp = int(left_start + i)
-			lp += 1
-			temp[ls_temp] = arr[lp]
-		
-		else:
-			if arr[lp] < arr[rp]:
-				ls_temp = int(left_start + i)
-				lp += 1
-				temp[ls_temp] = arr[lp]
-			else:
-				ls_temp = int(left_start + i)
-				rp += 1
-				temp[ls_temp] = arr[rp]
-				count += int(right_start - lp)
-				#sender.append()
-
+		sender += left_side[i:]
+		sender += right_side[j:]
+		return tuple([counter, sender])
 
 if __name__ == "__main__":
-	arr = [2, 4, 1, 3, 5]
+	arr = [5, 4, 3, 2, 1]
 	
 	brute_results = brute_force_inversions(arr)
-	print("Brute Force Results: ")
-	for i in range(len(brute_results)):
+	print("Brute Force Results: " + f"\nInversions Count: {brute_results[0]}")
+	for i in range(1, len(brute_results)):
 		print(f"{brute_results[i]}", end=" ")
 
 	optimized_results = optimal_inversions(arr)
-	print("Optimized Results: ")
-	for i in range(len(optimized_results)):
+	print("\n\nOptimized Results: " + f"\nInversions Count: {optimized_results[0]}")
+	for i in range(1, len(optimized_results)):
 		print(f"{optimized_results[i]}", end=" ")
