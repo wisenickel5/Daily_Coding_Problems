825fc2d92ed0e7e1fd4f5bbe5b8f7110ca38aaac
Dylan Alexander <dalexander@carterandclark.com>
2022-03-22 12:12:51 -0400

    More additions to Linked List basics

--- Categories/Basic Programming/SLinkedListOperations.py
+++ Categories/Basic Programming/SLinkedListOperations.py
@@ -128,25 +128,82 @@ class SLinkedList:
 		nodes.append("None")
 		return " -> ".join(str(x) for x in nodes)
 
-#-------- Merge 2 Linked Lists --------#
-def merge_2LL(l_l1: SLinkedList, l_l2: SLinkedList) -> SLinkedList:
-	if l_l1.head is None:
-		return l_l2
-	if l_l2.head is None:
-		return l_l1
-	
-	head_ptr = temp_ptr = Node()
-	while not (l_l1.head is None or l_l2.head is None):
-		if l_l1.head.data < l_l2.head.data:
-			curr_node = l_l1.head
+class Two_LinkedList_Operations():
+	def __init__(self, linked_list1: SLinkedList, linked_list2: SLinkedList) -> None:
+		self.LL1 = linked_list1
+		self.LL2 = linked_list2
 
-	return head_ptr.next
+	def merge_2LL(self) -> Node:
+		"""Merge 2 Sorted Linked Lists.
+		Reference: https://stackoverflow.com/questions/22507197/merging-two-sorted-linked-lists-into-one-linked-list-in-python
+
+		Args:
+			l_l1 (SLinkedList): Linked List 1
+			l_l2 (SLinkedList): Linked List 2
+
+		Returns:
+			SLinkedList: _description_
+		"""
+		LL1_head = self.LL1.head
+		LL2_head = self.LL2.head
+		
+		# If either of the linked lists are empty, return the one thats not empty
+		if LL1_head is None:
+			return self.LL2
+		if LL2_head is None:
+			return self.LL1
+		
+		# Create a temp_ptr to avoid additional checks in loop.
+		head_ptr = temp_ptr = Node(0)
+		while not (LL1_head is None or LL2_head is None):
+			if LL1_head.data < LL2_head.data:
+				curr_node = LL1_head # Remember current lowest node
+				LL1_head = LL1_head.next # Move on
+			else:
+				curr_node = LL2_head # Remember current lowest node
+				LL2_head = LL2_head.next # Move on
+			
+			# Mutate the node after we have moved on to the next node.
+			temp_ptr.next = curr_node
+			# Advance temp_ptr
+			temp_ptr = temp_ptr.next
+
+		temp_ptr.next = LL1_head or LL2_head
+
+		return head_ptr
+
+	def recurs_merge_2LL(self) -> SLinkedList:
+		LL1_head = self.LL1.head
+		LL2_head = self.LL2.head
+		
+		# If either of the linked lists are empty, return the one thats not empty
+		if LL1_head is None:
+			return self.LL2
+		if LL2_head is None:
+			return self.LL1
+
+		if (LL1_head.data < LL2_head.data):
+			LL1_head.next = self.recurs_merge_2LL(LL1_head.next, LL2_head)
+			return self.LL1
+		else:
+			LL2_head.next = self.recurs_merge_2LL(LL2_head.next, LL1_head)
+			return self.LL2
 
 if __name__ == "__main__":
+	print("#---- LL with chars ----#\n")
 	l_l = SLinkedList(nodes = ['a', 'b', 'c', 'd'])
 	l_l.add_first(Node('first'))
 	l_l.add_last(Node('last'))
 	l_l.add_before('b', Node('before b'))
 	l_l.add_after('c', Node('after c'))
 	l_l.remove_node('d')
-	print(l_l.__repr__())
+	print(l_l.__repr__() + "\n")
+
+	print("#---- LL with ints ----#\n")
+	ll2 = SLinkedList(nodes=[1,3,5,7])
+	print(ll2.__repr__())
+	ll3 = SLinkedList(nodes=[2,4,6,8])
+	print(ll3.__repr__() + "\nMerging Linked Lists...\n")
+	ll4_head = Two_LinkedList_Operations(ll2, ll3).merge_2LL()
+	print(ll4_head.next)
+
